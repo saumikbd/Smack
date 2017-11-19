20ed1c966e63e2b9ecacb25d6f9be42a0b6919c2
Sadman Sakib Saumik <>
2017-11-20 02:42:52 +0600

    register account api set

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success:Bool)->()
+
+//Urls
+let BASE_URL = "https://ssmack.herokuapp.com/v1/"
+let URL_REGISTER = BASE_URL+"account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannelVC"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
